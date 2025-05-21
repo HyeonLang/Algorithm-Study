190fd1f51d04befe5fde81e2ce32a829f451d808
HyeonLang <92206509+HyeonLang@users.noreply.github.com>
2025-05-21 10:12:52 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1059.29 ms, Memory: 23.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <vector>
+#include <set>
+#include <iostream>
+using namespace std;
+
+int solution(vector<int> elements) {
+    int answer = 0;
+    set<int> results;
+    int all_sum = 0;
+    for (auto e : elements)
+        all_sum += e;
+    results.insert(all_sum);
+    
+    for (int count = 1; count <= elements.size() / 2; count++)
+    {
+
+        for (int start = 0; start < elements.size(); start++)
+        {
+            int sum = 0;
+            for (int i = 0; i < count; i++)
+            {
+                int idx = (start + i) % elements.size();
+                sum += elements[idx];
+            }
+            
+            results.insert(sum);
+            results.insert(all_sum - sum);
+        }
+        cout << endl;
+    }
+    cout << endl << endl;
+    answer  = results.size();
+    
+    return answer;
+}
