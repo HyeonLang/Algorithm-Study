5e984521cc083d7bb3a1c3f4fabcc82659aa04f8
HyeonLang <92206509+HyeonLang@users.noreply.github.com>
2025-05-29 15:12:12 +0900

    [level 3] Title: 베스트앨범, Time: 0.08 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
@@ -0,0 +1,56 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <queue>
+#include <iostream>
+
+using namespace std;
+
+struct GenreInfo
+{
+    int plays_count = 0;
+    priority_queue<vector<int>> song_info = priority_queue<vector<int>>();
+    
+    bool operator<(const GenreInfo& other) const 
+    {
+        return plays_count < other.plays_count;
+    }
+};
+
+void compare_vec()
+{
+    
+}
+
+vector<int> solution(vector<string> genres, vector<int> plays) {
+    vector<int> answer;
+    
+    map<string, GenreInfo> songs;
+    for (int i = 0; i < genres.size(); i++)
+    {
+        songs[genres[i]].plays_count += plays[i];
+        songs[genres[i]].song_info.push({ plays[i], (-1 * i) });
+    }
+    
+    priority_queue<GenreInfo> play_counts;
+    for (auto& [key, value] : songs)
+    {
+        play_counts.push(value);
+    }
+    
+    while(!play_counts.empty())
+    {
+        GenreInfo genre_info = play_counts.top();
+        play_counts.pop();
+        
+        for (int i = 0; i < 2; i++)
+        {
+            if (genre_info.song_info.empty()) break;
+            
+            answer.push_back((-1 * genre_info.song_info.top()[1]));
+            genre_info.song_info.pop();
+        }
+    }
+    
+    return answer;
+}
