6d09bb5b4bd1867f67b3733f8a2232990435f2ce
HyeonLang <92206509+HyeonLang@users.noreply.github.com>
2025-06-10 10:46:38 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 50.36 ms, Memory: 115 MB -BaekjoonHub

---
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.cpp
@@ -0,0 +1,57 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<vector<int>> board, vector<vector<int>> skill) {
+    int answer = 0;
+    
+    vector<vector<int>> skill_board(board.size(), vector<int>(board[0].size() , 0));
+    
+    for (auto s : skill)
+    {
+        int type = s[0];
+        int r1 = s[1];
+        int c1 = s[2];
+        int r2 = s[3];
+        int c2 = s[4];
+        int degree = s[5];
+        
+        skill_board[r1][c1] += (type == 1) ? -1 * degree : degree;
+        if (r2 < skill_board.size() - 1)
+            skill_board[r2 + 1][c1] -= (type == 1) ? -1 * degree : degree;
+        
+        if (c2 == skill_board[0].size() - 1) continue;
+        
+        skill_board[r1][c2 + 1] -= (type == 1) ? -1 * degree : degree;
+        if (r2 < skill_board.size() - 1)
+            skill_board[r2 + 1][c2 + 1] += (type == 1) ? -1 * degree : degree;   
+    }
+    
+    
+    
+    for (int r = 0; r < board.size(); r++)
+    {
+        int score = 0;
+        for (int c = 0; c < board[r].size(); c++)
+        {
+            score += skill_board[r][c];
+            skill_board[r][c] = score;
+        }
+    }
+    
+    for (int c = 0; c < board[0].size(); c++)
+    {
+        int score = 0;
+        for (int r = 0; r < board.size(); r++)
+        {
+            score += skill_board[r][c];
+            board[r][c] += score;
+            if (board[r][c] > 0) answer++;
+        }
+    }
+    
+    
+    return answer;
+}
