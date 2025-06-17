27469ed16355246aae8244a9d34f97d229cfc627
HyeonLang <92206509+HyeonLang@users.noreply.github.com>
2025-06-17 12:33:10 +0900

    [level 4] Title: 사칙연산, Time: 1.54 ms, Memory: 3.87 MB -BaekjoonHub

---
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.cpp
@@ -0,0 +1,50 @@
+#include <vector>
+#include <string>
+#include <iostream>
+using namespace std;
+
+int dp[101][101][2];
+string op[100];
+int solution(vector<string> arr)
+{
+    int answer = -1;
+    
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (i % 2 == 1) 
+        {
+            op[(i / 2)] = arr[i];
+        }
+        else 
+        {
+            dp[i / 2][i / 2][0] = stoi(arr[i]);
+            dp[i / 2][i / 2][1] = stoi(arr[i]);
+        }
+    }
+    
+    for (int gap = 1; gap <= arr.size() / 2; gap++)
+    {
+        for (int i = 0; i + gap <= arr.size() / 2; i++)
+        {
+            dp[i][i + gap][1] = (gap + 1) * -1001;
+            dp[i][i + gap][0] = (gap + 1) * 1001;
+            for (int k = i; k < i + gap; k++)
+            {
+                if (op[k] == "+")
+                {
+                    dp[i][i + gap][1] = max(dp[i][i + gap][1], dp[i][k][1] + dp[k + 1][i + gap][1]);
+                    dp[i][i + gap][0] = min(dp[i][i + gap][0], dp[i][k][0] + dp[k + 1][i + gap][0]);
+                }
+                else
+                {
+                    dp[i][i + gap][1] = max(dp[i][i + gap][1], dp[i][k][1] - dp[k + 1][i + gap][0]);
+                    dp[i][i + gap][0] = min(dp[i][i + gap][0], dp[i][k][0] - dp[k + 1][i + gap][1]); 
+                }
+            }
+            //cout << "("<<dp[i][i + gap][1] << ", " << dp[i][i + gap][0] << ") "; 
+        }
+        //cout << endl;
+    }
+    answer = dp[0][arr.size() / 2][1];
+    return answer;
+}
