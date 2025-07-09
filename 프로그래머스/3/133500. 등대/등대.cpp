c7070105689a71107efe75cb0a7df828deaaae8d
HyeonLang <92206509+HyeonLang@users.noreply.github.com>
2025-07-09 09:29:23 +0900

    [level 3] Title: 등대, Time: 33.90 ms, Memory: 27.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/133500. 등대/등대.cpp
@@ -0,0 +1,41 @@
+#include <string>
+#include <vector>
+#include <deque>
+#include <set>
+#include <algorithm>
+#include <iostream>
+#include <queue>
+using namespace std;
+
+void solve(int light, vector<vector<int>>& load, vector<int>& visit)
+{
+    visit[light] = 0;
+    for (auto child : load[light])
+    {
+        if (visit[child] != -1) continue;
+        
+        solve(child, load, visit);
+        
+        if (visit[child] == 0) visit[light] = 1; 
+    }
+}
+
+
+int solution(int n, vector<vector<int>> lighthouse) {
+    int answer = 0;
+    vector<int> visit(n + 1, -1);
+    vector<vector<int>> load(n + 1, vector<int>());
+    
+    for (auto l : lighthouse)
+    {
+        load[l[0]].push_back(l[1]);
+        load[l[1]].push_back(l[0]);
+    }
+    
+    solve(1, load, visit);
+    
+    for (auto v : visit)
+        if (v == 1) answer++;
+   
+    return answer;
+}
